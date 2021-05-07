e4273bdafed1adc448aacdae84973f31412d2af8
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2021-05-07 18:46:20 +0100

    function was made to update song and its data

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const progress = document.querySelector('.progress')
 const progressContainer = document.querySelector('.progress-container')
 const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
+
+
+//song titles
+const songs =['hey', 'summer', 'ukulele']
+
+//keep track of songs
+let songIndex = 2
+
+
+// initially load songs into DOM - we create a function that takes songs array with song index
+loadSong(songs[songIndex])
+
+
+// Update song details
+function loadSong(song) {
+    //here we change the title
+    title.innerText = song
+    // here we change the song and format should be mp3 we give the address to the song
+    audio.src = `music/${song}.mp3`
+    //here we change the cover
+    cover.src = `images/${song}.jpg`
+
+}
